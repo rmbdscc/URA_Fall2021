d24158ad9b2e7255dc8f257c0c6440511f0df345
Jing Liu <j762liu@uwaterloo.ca>
2021-11-16 00:34:59 -0500

    Able to create customized hierarchy

--- gradle_project/src/main/java/HelloWorld.java
+++ gradle_project/src/main/java/HelloWorld.java
@@ -3,13 +3,13 @@ public class HelloWorld {
 
     public static String getGreeting() {
         String message = "";
-        //#if isJava8==true
-        message += "Java8!" + "\n";
-        //#elseif isJava11==true
-        message += "Java11!" + "\n";
+
+        //#if isJava8
+        message += "Java8 is true!";
         //#else
-        message += "Not Java8 nor Java11" + "\n";
+        message += "Not Java 8!";
         //#endif
+
         return message;
     }
 
